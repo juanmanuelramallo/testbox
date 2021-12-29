4337719cf8edecd013b8f9198c1c4a5a7289b83c
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2021-12-29 17:00:00 -0400

    Add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate
+
+  protected
+
+  def authenticate
+    http_basic_authenticate_or_request_with(name: "testbox", password: ENV.fetch("USER_PASSWORD"))
+  end
 end
