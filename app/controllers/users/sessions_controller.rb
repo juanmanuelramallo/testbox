0457e6018e4346ac0e2b1b62d3050630454808fe
Juan Manuel Ramallo <juanmanuelramallo@hey.com>
2022-01-06 21:20:13 -0400

    Fix login/logout redirection, empty state for actions partial

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -23,7 +23,11 @@ class Users::SessionsController < Devise::SessionsController
   protected
 
   def after_sign_out_path_for(resource_or_scope)
-    new_user_session_path
+    root_path
+  end
+
+  def after_sign_in_path_for(resource)
+    root_path
   end
 
   # If you have extra params to permit, append them to the sanitizer.
